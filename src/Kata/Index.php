34b76e4662a0f6dff409caf61f35db169cbb3d7a
pooria <pooria@drsnwk.com>
2021-02-12 20:00:41 -0500

    i

--- src/Kata/Index.php
+++ src/Kata/Index.php
@@ -8,4 +8,31 @@ class Index
     {
         return true;
     }
+    
+    public function printOneToHundred(){
+
+        for($number=1; $number<=100; $number++){
+
+            if($number%15 === 0){
+                echo "FIZZBUZZ <br>";
+                continue;
+            }
+            if($number%3 === 0){
+                echo "FIZZ <br>";
+                continue;
+            }
+            if($number%5 === 0){
+                echo "BUZZ <br>";
+                continue;
+            }
+            echo "$number<br>";
+        }
+    
+    }
+ 
 }
+
+$numbers = new Index();
+$numbers->printOneToHundred();
+
+
