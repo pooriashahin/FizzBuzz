6b4b37fb6db780e390b69a4195b508a1b6d0e00b
pooria <pooria@drsnwk.com>
2021-02-13 19:31:14 -0500

    i

--- src/Kata/solution.php
+++ src/Kata/solution.php
@@ -7,13 +7,9 @@ $i = new Fizzbuzz();
 
 for($number=1; $number<=100; $number++){
 
-    
-    
     $i->printNumber($number);
     $i->replaceFactorOfThree($number);
     $i->replaceFactorOfFive($number);
     $i->replaceFactorOfThreeAndFive($number);
     
     }
-
-
